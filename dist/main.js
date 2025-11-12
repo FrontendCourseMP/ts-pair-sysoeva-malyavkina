89328286e283b1848dca00d49d303af60c498e2d
Margo <djessi.real@gmail.com>
2025-11-12 21:59:09 +0300

    Малявкина

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,2 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const form_handler_1 = require("./form-handler");
-(0, form_handler_1.setupForm)();
-
-import { setupForm } from './form-handler.js';
+import { setupForm } from '../src/form-handler';
 setupForm();
